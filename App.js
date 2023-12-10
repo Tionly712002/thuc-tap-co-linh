62318df282469aea773679e2970fe54e539ac7a8
Tionly712002 <nguyenhuuduc712002@gmail.com>
2023-12-10 22:30:19 +0700

    ban0.1

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+// App.js
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { addUser, getUserByUsername, updateUserPassword, deleteUser } from './database';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    addUser(username, password, (result) => {
+      if (result.rowsAffected > 0) {
+        Alert.alert('Success', 'User registered successfully!');
+      } else {
+        Alert.alert('Error', 'Failed to register user.');
+      }
+    });
+  };
+
+  const handleLogin = () => {
+    getUserByUsername(username, (result) => {
+      if (result.rows.length > 0) {
+        const storedPassword = result.rows.item(0).password;
+        if (storedPassword === password) {
+          Alert.alert('Success', 'Login successful!');
+        } else {
+          Alert.alert('Error', 'Incorrect password.');
+        }
+      } else {
+        Alert.alert('Error', 'User not found.');
+      }
+    });
+  };
+
+  const handleUpdatePassword = () => {
+    updateUserPassword(username, 'newpassword', (result) => {
+      if (result.rowsAffected > 0) {
+        Alert.alert('Success', 'Password updated successfully!');
+      } else {
+        Alert.alert('Error', 'Failed to update password.');
+      }
+    });
+  };
+
+  const handleDeleteUser = () => {
+    deleteUser(username, (result) => {
+      if (result.rowsAffected > 0) {
+        Alert.alert('Success', 'User deleted successfully!');
+      } else {
+        Alert.alert('Error', 'Failed to delete user.');
+      }
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TextInput placeholder="Username" onChangeText={setUsername} />
+      <TextInput placeholder="Password" onChangeText={setPassword} secureTextEntry />
+      <Button title="Sign Up" onPress={handleSignUp} />
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="Update Password" onPress={handleUpdatePassword} />
+      <Button title="Delete User" onPress={handleDeleteUser} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Database.js
@@ -0,0 +1,47 @@
+// database.js
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('mydatabase.db');
+
+// Tạo bảng người dùng
+db.transaction(tx => {
+  tx.executeSql(
+    'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);'
+  );
+});
+
+// Thêm người dùng
+export const addUser = (username, password, callback) => {
+  db.transaction(tx => {
+    tx.executeSql('INSERT INTO users (username, password) VALUES (?, ?);', [username, password], (_, result) => {
+      callback(result);
+    });
+  });
+};
+
+// Lấy người dùng theo tên đăng nhập
+export const getUserByUsername = (username, callback) => {
+  db.transaction(tx => {
+    tx.executeSql('SELECT * FROM users WHERE username = ?;', [username], (_, result) => {
+      callback(result);
+    });
+  });
+};
+
+// Cập nhật mật khẩu người dùng
+export const updateUserPassword = (username, newPassword, callback) => {
+  db.transaction(tx => {
+    tx.executeSql('UPDATE users SET password = ? WHERE username = ?;', [newPassword, username], (_, result) => {
+      callback(result);
+    });
+  });
+};
+
+// Xóa người dùng
+export const deleteUser = (username, callback) => {
+  db.transaction(tx => {
+    tx.executeSql('DELETE FROM users WHERE username = ?;', [username], (_, result) => {
+      callback(result);
+    });
+  });
+};
